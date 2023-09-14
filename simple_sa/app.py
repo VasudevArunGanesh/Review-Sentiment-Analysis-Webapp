47207bff562b4b258b9e63406644a156f306c5c0
VasudevArunGanesh <117260954+VasudevArunGanesh@users.noreply.github.com>
2023-09-14 18:16:37 +0530

    Add files via upload

--- simple_sa/app.py
+++ simple_sa/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
+from textblob import TextBlob
+
 
 nltk.download('vader_lexicon')
 
@@ -12,10 +14,15 @@ def main():
         inp = request.form.get("inp")
         sid = SentimentIntensityAnalyzer()
         score = sid.polarity_scores(inp)
-        if score["neg"]!=0:
-            return render_template('index.html',message="negative")
+        #message = "Positive" if score['compound'] > 0 else "Negative" if score['compound'] < 0 else "Neutral"
+        analysis = TextBlob(inp)
+        sentiment = analysis.sentiment
+        if score["compound"]>0:
+            return render_template('index.html',message="Positive",sentiment=sentiment,sentiment_score=score['compound'],input=inp)
+        elif score["compound"]<0:
+            return render_template('index.html', message = "Negative",sentiment=sentiment,sentiment_score=score['compound'],input=inp)
         else:
-            return render_template('index.html', message = "positive")
+            return render_template('index.html', message = "Neutral",sentiment=sentiment,sentiment_score=0.0,input=inp)
     return render_template('index.html')
 
 if __name__ == '__main__':
